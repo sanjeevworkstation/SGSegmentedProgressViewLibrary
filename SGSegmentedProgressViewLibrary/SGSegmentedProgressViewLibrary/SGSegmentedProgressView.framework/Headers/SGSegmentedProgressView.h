02763eefaad3c7724ddf1046389cfd460ecd8ccd
Sanjeev Kumar Gautam <sanjeev.gautam@nykaa.com>
2020-05-27 13:14:32 +0530

    add version 0.0.1

---
+++ SGSegmentedProgressViewLibrary/SGSegmentedProgressViewLibrary/SGSegmentedProgressView.framework/Headers/SGSegmentedProgressView.h
@@ -0,0 +1,19 @@
+//
+//  SGSegmentedProgressView.h
+//  SGSegmentedProgressView
+//
+//  Created by Sanjeev Gautam on 27/05/20.
+//  Copyright © 2020 SG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SGSegmentedProgressView.
+FOUNDATION_EXPORT double SGSegmentedProgressViewVersionNumber;
+
+//! Project version string for SGSegmentedProgressView.
+FOUNDATION_EXPORT const unsigned char SGSegmentedProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGSegmentedProgressView/PublicHeader.h>
+
+
---
+++ SGSegmentedProgressViewLibrary/SGSegmentedProgressViewLibrary/SGSegmentedProgressViewLibrary.h
@@ -0,0 +1,19 @@
+//
+//  SGSegmentedProgressViewLibrary.h
+//  SGSegmentedProgressViewLibrary
+//
+//  Created by Sanjeev Gautam on 27/05/20.
+//  Copyright © 2020 SG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SGSegmentedProgressViewLibrary.
+FOUNDATION_EXPORT double SGSegmentedProgressViewLibraryVersionNumber;
+
+//! Project version string for SGSegmentedProgressViewLibrary.
+FOUNDATION_EXPORT const unsigned char SGSegmentedProgressViewLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGSegmentedProgressViewLibrary/PublicHeader.h>
+
+
